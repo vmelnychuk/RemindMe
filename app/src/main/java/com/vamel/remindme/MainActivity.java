361da5d626caf8fadd289ba8d9892f12b7273b16
Vasyl Melnychuk <vmmelnychuk@gmail.com>
2015-06-06 00:53:24 +0300

    add Toolbar nad icon for it

--- app/src/main/java/com/vamel/remindme/MainActivity.java
+++ app/src/main/java/com/vamel/remindme/MainActivity.java
@@ -3,14 +3,31 @@ package com.vamel.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends Activity {
+    private Toolbar toolBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolBar = (Toolbar) findViewById(R.id.toolbar);
+        toolBar.setTitle(R.string.app_name);
+        toolBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+        toolBar.inflateMenu(R.menu.menu);
     }
 }
